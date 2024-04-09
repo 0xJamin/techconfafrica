57bb71916bcc4f2e5c8cff280df43d5002eb66f5
Jamin <okpusjamin@gmail.com>
2024-04-09 19:22:54 +0100

    feat(TCA-24): add newsletter component

--- lint-staged.cjs
+++ lint-staged.cjs
@@ -2,6 +2,6 @@ module.exports = {
   "*.{ts,tsx,astro}": ["eslint --max-warnings=0 --fix"],
   "*.{ts,tsx}": ["tsc --skipLibCheck --noEmit"],
   "*.{ts,tsx,json,css,js, astro}": [
-    "prettier --write --plugin-search-dir=. --plugin=prettier-plugin-astro",
+    "prettier --write --plugin-search-dir=. --plugin=prettier-plugin-astro ",
   ],
 };
