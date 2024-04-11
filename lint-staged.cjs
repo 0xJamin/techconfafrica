6d221560d9af988b8a14d5809cd75bb6e29856d3
Jamin <okpusjamin@gmail.com>
2024-04-11 13:10:14 +0100

    chore(TCA-24): remove comments

--- lint-staged.cjs
+++ lint-staged.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   "*.{ts,tsx,astro}": ["eslint --max-warnings=0 --fix"],
-  // "*.{ts,tsx}": ["tsc -p tsconfig.json --noEmit"],
   "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
   "*.{ts,tsx,json,css, astro}": [
     "prettier --write --plugin-search-dir=. --plugin=prettier-plugin-astro ",
